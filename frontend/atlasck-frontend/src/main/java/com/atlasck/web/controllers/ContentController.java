1f2f0a6bc44e79385f1be6463de2a997585bd90f
Georgi Lambov <dev@uvwebs.com>
2010-11-18 06:13:22 +0200

    Added: tiles views, jspx layout

--- frontend/atlasck-frontend/src/main/java/com/atlasck/web/controllers/ContentController.java
+++ frontend/atlasck-frontend/src/main/java/com/atlasck/web/controllers/ContentController.java
@@ -2,22 +2,19 @@ package com.atlasck.web.controllers;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/content/**")
 public class ContentController {
 
 	protected final Log logger = LogFactory.getLog(getClass());
 
-	@RequestMapping(method=RequestMethod.GET, value="about-us")
-	@ResponseStatus(value=HttpStatus.OK)
-	public ModelAndView getAboutUs(ModelMap modelMap) {
-		return new ModelAndView("content/aboutus");
+	@RequestMapping(value="about-us", method=RequestMethod.GET)
+	public String getAboutUs(ModelMap modelMap) {
+		return "content/aboutus";
 	}
 }
