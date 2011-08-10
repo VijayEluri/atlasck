6eaf759d09f51ba2a14330c5e020cfc55cfaff33
georgi <dev@uvwebs.com>
2011-08-10 04:44:32 +0300

    Added: Comment in interface

--- frontend/atlasck-frontend/src/main/java/com/atlasck/repository/AnswerRepo.java
+++ frontend/atlasck-frontend/src/main/java/com/atlasck/repository/AnswerRepo.java
@@ -2,5 +2,11 @@ package com.atlasck.repository;
 
 import com.atlasck.domain.Answer;
 
+/**
+ * Answer interface
+ * 
+ * @author Georgi Lambov
+ * 
+ */
 public interface AnswerRepo extends AbstractRepo<Answer> {
 }
