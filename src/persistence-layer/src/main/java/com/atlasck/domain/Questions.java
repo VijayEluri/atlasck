dafc1344ecf05230e9553a0d74685773eebab09d
gogoluxecs <gogo_luxecs@yahoo.com>
2012-09-19 22:42:52 +0300

    Added domain model reversed engineered.

---
+++ src/persistence-layer/src/main/java/com/atlasck/domain/Questions.java
@@ -0,0 +1,13 @@
+package com.atlasck.domain;
+
+import org.springframework.roo.addon.dbre.RooDbManaged;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.entity.RooJpaEntity;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaEntity(versionField = "", table = "questions")
+@RooDbManaged(automaticallyDelete = true)
+public class Questions {
+}
---
+++ src/persistence-layer/src/test/java/com/atlasck/domain/VisitorsDataOnDemand.java
@@ -0,0 +1,7 @@
+package com.atlasck.domain;
+
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = Visitors.class)
+public class VisitorsDataOnDemand {
+}
